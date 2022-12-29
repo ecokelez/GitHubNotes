a869b64864bd7cd97fab33cd8f9013122634fa42
ecokelez <ersoy.cokelez@gmail.com>
2022-12-29 20:30:29 +0200

    GitHub_Note

---
+++ src/C01_GitHub.java
@@ -0,0 +1,11 @@
+public class C01_GitHub {
+    public static void main(String[] args) {
+        System.out.println("Projeleri push yapamıyorum,sürekli hata alıyorum!..");
+        System.out.println("Projeleri push yapamıyorum,sürekli hata alıyorum!..");
+        System.out.println("Projeleri push yapamıyorum,sürekli hata alıyorum!..");
+        System.out.println("Projeleri push yapamıyorum,sürekli hata alıyorum!..");
+        System.out.println("Projeleri push yapamıyorum,sürekli hata alıyorum!..");
+        System.out.println("Projeleri push yapamıyorum,sürekli hata alıyorum!..");
+        System.out.println("Projeleri push yapamıyorum,bu defa oldu gibi ...");
+    }
+}
---
+++ src/C02_GitHubNotes.java
@@ -0,0 +1,36 @@
+public class C02_GitHubNotes {
+    public static void main(String[] args) {
+        /*
+        GitHub Notes:
+                İlk defa PUSH yapacaksanız bir projeyi ;
+        1 - Önce GitHub a gidip New Repository i oluşturun
+        2- Oluşturduğunuz repository de size verilen https://github.com/.............git   adresini kopyalayın
+        Şimdi IntelliJ ye gelip TERMINAL ı açın;
+        3- git init yazın
+        4- git add . yazın
+        5- git commit -m"first commit"  (first commit yerine  ilke versiyon, ilk ders veya istediğiniz bir şeyi yazın)
+        6- git branch -M main
+        7- git remote add origin https://github.com/......git (yukarıda2. satırda kopyalağınız adresi buraya yapıştırın)
+        8- git push -u origin main
+
+        SONRASINDA Aynı PROJE için ;
+        her yeni eklediğiniz şeyden sonra GitHub'a yüklemek için
+        9- git add .
+        10 - git commit -m"YENI NUMARA VEYA YAZI YAZ"
+        11 - git push
+        git remote add origin adres
+        git push -u origin master
+        NOT: Yukaridaki iki komutu tek seferde kullandiktan sonra ikinci commitlerde sadece git push kullaniriz,
+        hepsi bu kadar !
+        
+     git log --oneline ==> Commitlerdeki yani versiyonlardaki durumu gösterir
+            Not : Master Branch'inde yeni veri ekleme, dosya ekleme ve degistirme ...
+
+       NOT : ==> git config --global credential.helper store username Git push login sorunu icin
+
+
+
+
+         */
+    }
+}
