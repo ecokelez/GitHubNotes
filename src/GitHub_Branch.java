5e63cba09cf8ebe5045fea5d91aad638f8b03a72
ecokelez <ersoy.cokelez@gmail.com>
2022-12-30 15:49:14 +0200

    branch

---
+++ src/GitHub_Branch.java
@@ -0,0 +1,21 @@
+public class GitHub_Branch {
+    //Branch'in faydalari
+    // Orginal kodların güvenliği sağlanır
+    // Her developer kendi bölümünden sorumlu olur
+    // Daha hızlı geliştirme yapılır
+    // Daha az hata oluşur
+    // Sorunlar daha hızlı düzeltilir.
+    // Organize kod yapısı sağlanır
+    // Kaos olmaz
+
+       /*
+        git branch [isim] ==> Yeni branch oluşturur
+        git branch ==>Mevcut branch leri listeler
+        git checkout [isim] ==> Branch aktif hale gelir
+        git branch –d [isim] ==> Branch i siler
+        git merge [isim] ==> İki branch i birleştirir
+        git branch -m [isim] ==> Branch ismini değiştirir.
+       */
+
+
+}
---
+++ src/Stash.java
@@ -0,0 +1,15 @@
+public class Stash {
+
+    /*
+    Stashing
+          git stash
+       Working directory ve stage area daki –henüz commit haline gelmemiş değişikliklerin
+       geçici olarak geri alınması için stashing işlemi yapılır.
+            git stash list
+       Working space ve  staging areadaki değişiklikleri geçici
+       olarak hafızaya alır ve bu bölgeleri temizler
+           git stash pop
+       Hafızaya alınan değişiklikleri geri uygulamak için kullanılır.
+
+     */
+}
