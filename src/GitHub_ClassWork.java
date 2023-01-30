af1cee6d415fc4667973b90a6f983275f1f864f6
ecokelez <ersoy.cokelez@gmail.com>
2023-01-30 12:19:36 +0200

    c.2

--- src/GitHub_ClassWork.java
+++ src/GitHub_ClassWork.java
@@ -2,6 +2,7 @@ public class GitHub_ClassWork {
 
     public static void main(String[] args) {
         System.out.println("GitHub ClassWork 30 Ocak Pazartesi ");
+        System.out.println("GitHub ClassWork 30 Ocak Pazartesi- 1 ");
     }
 
 }
