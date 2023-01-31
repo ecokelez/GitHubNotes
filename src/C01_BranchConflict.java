e19eb47709c08d6ddafed88e5a14bfda50cbced0
ecokelez <ersoy.cokelez@gmail.com>
2023-01-31 14:07:56 +0200

    con02

--- src/C01_BranchConflict.java
+++ src/C01_BranchConflict.java
@@ -1,5 +1,6 @@
 public class C01_BranchConflict {
     public static void main(String[] args) {
         System.out.println("Branch Conflict deneme 1");
+        System.out.println("Branch Conflict deneme 2.");
     }
 }
