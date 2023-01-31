a587b4fa4a50c0c0dd4a0965b80572638fcce85f
ecokelez <ersoy.cokelez@gmail.com>
2023-01-31 13:38:12 +0200

    con2

--- src/C03_GitGitHub.java
+++ src/C03_GitGitHub.java
@@ -1,5 +1,6 @@
 public class C03_GitGitHub {
     public static void main(String[] args) {
         System.out.println("GitHub branch ve confklict calisma");
+        System.out.println("GitHub ersoy- branch  ve confklict calisma");
     }
 }
