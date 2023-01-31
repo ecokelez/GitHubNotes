fc231c16d5071711fdf5b9a5c516925decbd511e
ecokelez <ersoy.cokelez@gmail.com>
2023-01-31 13:36:14 +0200

    Con01

---
+++ src/C01_BranchConflict.java
@@ -0,0 +1,5 @@
+public class C01_BranchConflict {
+    public static void main(String[] args) {
+        System.out.println("Branch Conflict deneme 1");
+    }
+}
--- src/GitHub_ClassWork.java
+++ src/GitHub_ClassWork.java
@@ -2,7 +2,7 @@ public class GitHub_ClassWork {
 
     public static void main(String[] args) {
         System.out.println("GitHub ClassWork 30 Ocak Pazartesi ");
-        System.out.println("GitHub ClassWork 30 Ocak Pazartesi- 1 ");
+        System.out.println("GitHub ClassWork 30 Ocak Pazartesi-1 ");
     }
 
 }
